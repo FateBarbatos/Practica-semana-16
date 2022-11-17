79eb3ebbf258de93b7b920216a73360fab96ca70
Fate <Fate@DESKTOP-QKB1521>
2022-11-17 12:58:11 +0100

    Agregar archivos de proyecto.

---
+++ Practica semana 16/Controllers/PracticaPrograController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_semana_16.Controllers
+{
+    public class PracticaPrograController : Controller
+    {
+        public IActionResult Paramethers(string empresaName, string propietarioName)
+        {
+            ViewData["empresa"] = empresaName;
+            ViewData["Name"] = "Hola " + propietarioName + " eres el dueño de la empresa:" + empresaName;
+            return View();
+        }
+    }
+}
---
+++ Practica semana 16/Data/Semana16Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practica_semana_16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_semana_16.Data
+{
+    public class Semana16Context : DbContext
+    {
+        public Semana16Context(DbContextOptions<Semana16Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+    }
+}
